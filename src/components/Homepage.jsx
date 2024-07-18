07460f4e565323a7bd47d7b8758480369904c8bc
Hassan <hassanhauda@gmail.com>
2024-07-18 06:49:33 +0100

    refactor: Remove commented code and unnecessary CSS class in Homepage component

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -8,14 +8,13 @@ import Navbar from "./Navbar";
 const Homepage = () => {
   return (
     <>
-      <main className="h-[881px] w-full bg-gradient-to-b from-[#dbf0ff] to-white dark:h-full dark:bg-gradient-to-b dark:from-[#2a3d5f] dark:to-[#253146]">
-        <Navbar />
-        <Hero />
-        <Info />
-        <Documentation />
-        <Base />
-        <Footer />
-      </main>
+      <Navbar />
+      <Hero />
+      <Info />
+      <Documentation />
+      <Base />
+      <Footer />
+      {/* </main> */}
     </>
   );
 };
