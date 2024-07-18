65c59609b8ad52ac2177abb3d6e08c0c257e7b6b
Hassan <hassanhauda@gmail.com>
2024-07-18 06:28:59 +0100

    refactor: Update main container styling in Homepage component

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -8,7 +8,7 @@ import Navbar from "./Navbar";
 const Homepage = () => {
   return (
     <>
-      <main className="h-[881px] w-full overflow-x-hidden bg-gradient-to-b from-[#dbf0ff] to-white dark:h-full dark:bg-gradient-to-b dark:from-[#2a3d5f] dark:to-[#253146]">
+      <main className="h-[881px] w-full bg-gradient-to-b from-[#dbf0ff] to-white dark:h-full dark:bg-gradient-to-b dark:from-[#2a3d5f] dark:to-[#253146]">
         <Navbar />
         <Hero />
         <Info />
